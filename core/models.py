64dcdcebf27776daaed016cf5b86d8c44f442109
Francisca <raebeb@gmail.com>
2020-11-22 22:07:40 -0300

    models ok

--- core/models.py
+++ core/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'Doctor'
+        verbose_name_plural = 'Doctors'
+
+    def __str__(self):
+        return (self.name)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    address = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'Patient'
+        verbose_name_plural = 'Patients'
+
+    def __str__(self):
+        return("Paciente: {} {}". format(self.name , self.lastname))
+
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        verbose_name = 'Appointment'
+        verbose_name_plural = 'Appointments'
+
+    def __str__(self):
+        return ("Sr/Sra. {} su reserva creada para el {} a las {} con el doctor {}".format(self.patient.name, self.date, self.time, self.doctor.name))
+
--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("index view")
+
+def appointment(request):
+    return HttpResponse("appointment view")
+
+def success(request):
+    return HttpResponse("succ view")
